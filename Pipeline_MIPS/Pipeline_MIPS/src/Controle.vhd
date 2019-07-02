3c3e81d58362be64b446e427d7c522b1d4ebef72
davidengelstein <davidengelstein@hotmail.com>
2019-07-02 00:45:46 -0300

    Geralzao

---
+++ Pipeline_MIPS/Pipeline_MIPS/src/Controle.vhd
@@ -0,0 +1,47 @@
+-------------------------------------------------------------------------------
+--
+-- Title       : Fub1
+-- Design      : Pipeline_MIPS
+-- Author      : Davidengelstein
+-- Company     : Davidengelstein
+--
+-------------------------------------------------------------------------------
+--
+-- File        : c:\Users\david\OneDrive\Documentos\GitHub\Pipeline_orgarq\Pipeline_MIPS\Pipeline_MIPS\src\Controle.vhd
+-- Generated   : Mon Jul  1 22:48:38 2019
+-- From        : interface description file
+-- By          : Itf2Vhdl ver. 1.22
+--
+-------------------------------------------------------------------------------
+--
+-- Description : 
+--
+-------------------------------------------------------------------------------
+
+--{{ Section below this comment is automatically maintained
+--   and may be overwritten
+--{entity {Fub1} architecture {Controle}}
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity Fub1 is
+	 port(
+		 OpCode : in STD_LOGIC_VECTOR(5 downto 0);
+		 Controle : out STD_LOGIC_VECTOR(9 downto 0)
+	     );
+end Fub1;
+
+--}} End of automatically maintained section
+
+architecture Controle of Fub1 is
+begin
+
+	Controle <= "1100000010" when OpCode = "000000" else    --tipo R
+				"0001001011" when OpCode = "100011" else    --load (lw)
+				"X00100010X" when OpCode = "101011"	else	--store (sw)
+				"X01010000X" when OpCode = "000100" else    -- beq
+				"X01001000X" when OpCode = "000010" else	-- jump	 
+				"0000000000";
+
+end Controle;
--- Pipeline_MIPS/Pipeline_MIPS/src/Controle_ALU.vhd
+++ Pipeline_MIPS/Pipeline_MIPS/src/Controle_ALU.vhd
@@ -28,7 +28,7 @@ use IEEE.std_logic_1164.all;
 entity Fub2 is
 	 port(
 		 ALUOp : in STD_LOGIC_VECTOR(1 downto 0);
-		 Controle_ALU : out STD_LOGIC_VECTOR(3 downto 0);
+		 Controle_ALU : out STD_LOGIC_VECTOR(2 downto 0);
 		 funct : in STD_LOGIC_VECTOR(5 downto 0)
 	     );
 end Fub2;
@@ -37,13 +37,13 @@ end Fub2;
 
 architecture Fub2 of Fub2 is
 begin
-	Controle_ALU <= "0010" when ALUOp = "00" else
-					"0110" when ALUOp = "X1" else
-					"0010" when ALUOp = "1X" and funct = "XX0000" else
-					"0110" when ALUOp = "1X" and funct = "XX0010" else
-					"0000" when ALUOp = "1X" and funct = "XX0100" else
-					"0001" when ALUOp = "1X" and funct = "XX0101" else
-					"0111" when ALUOp = "1X" and funct = "XX1010";
+	Controle_ALU <= "010" when ALUOp = "00" else
+					"110" when ALUOp = "X1" else
+					"010" when ALUOp = "1X" and funct = "XX0000" else
+					"110" when ALUOp = "1X" and funct = "XX0010" else
+					"000" when ALUOp = "1X" and funct = "XX0100" else
+					"001" when ALUOp = "1X" and funct = "XX0101" else
+					"111" when ALUOp = "1X" and funct = "XX1010";
 	 -- enter your statements here --
 
 end Fub2;
